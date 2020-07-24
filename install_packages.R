204245c8dc9b55b6167e3199dc767e14f39343e4
alakob <francoisalako@yahoo.co.uk>
2020-07-24 15:20:46 +0100

    Support export of mapview data

--- install_packages.R
+++ install_packages.R
@@ -1,2 +1,2 @@
-install.packages(c("maps","ggrepel","viridis","flexdashboard","curl","ggstance","ggalt","highcharter","lubridate","rjson","rmarkdown","remotes"), repos = "http://cran.uk.r-project.org")
+install.packages(c("XML","rlist","maps","ggrepel","viridis","flexdashboard","curl","ggstance","ggalt","highcharter","lubridate","rjson","rmarkdown","remotes"), repos = "http://cran.uk.r-project.org")
 remotes::install_github("juliasilge/silgelib")
